6799917283c7094cbe96cfee6a500561962c2ab0
Ratnesh <pillairatnesh96@gmail.com>
2024-07-19 22:28:31 +0530

    try one

--- Backend/index.js
+++ Backend/index.js
@@ -23,8 +23,7 @@ app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 
 const PORT = 8080 || process.env.PORT;
 
-app.get("/", (req, res) => {
-  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
 

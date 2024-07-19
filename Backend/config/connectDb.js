d1079e0ee7272b8c19df213606e8db3b98f76a91
Ratnesh <pillairatnesh96@gmail.com>
2024-07-19 11:26:29 +0530

    one commit

--- Backend/config/connectDb.js
+++ Backend/config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/ExpenseDB");
+    await mongoose.connect("mongodb://127.0.0.1:27017/ExpenseDB");
     console.log("Server Running On Mongodb");
   } catch (error) {
     console.log(`${error}`.bgRed);
